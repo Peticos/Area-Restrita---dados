a23ea4c4a189032cbe50eaee26e61d56525fd49e
GeoRPOliveira <124629573+GeoRPOliveira@users.noreply.github.com>
2024-11-05 13:14:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pickle
 from script import predict
 
@@ -17,6 +17,10 @@ def publico_alvo():
 def home():
     return render_template('index.html')  # Renders the HTML page
 
+@app.route('/')
+def index():
+    return redirect(url_for("home"))
+
 @app.route('/previsao-user', methods=['POST'])
 def receber_dados():
     dados = request.json.get('dados')
@@ -29,4 +33,4 @@ def receber_dados():
     return jsonify({'percentage': result[0], 'would_use': int(result[1])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
